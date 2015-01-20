a5f25351db25eff79351b48ec445a6ecc3fa3491
lzz <arloou@gmail.com>
2015-01-20 19:19:15 +0800

    Initial Commit

---
+++ EncryptBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptBox
+//
+//  Created by ucs on 15/1/20.
+//  Copyright (c) 2015年 lzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EncryptBox/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EncryptBox
+//
+//  Created by ucs on 15/1/20.
+//  Copyright (c) 2015年 lzz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
